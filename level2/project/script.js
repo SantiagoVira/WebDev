d358c32b9e6c8b8e5e09b719cdb661a9a7285cd4
SantiagoVira <santiagovira06@gmail.com>
2023-09-20 10:16:47 -0400

    cats

--- level2/project/script.js
+++ level2/project/script.js
@@ -1,1 +1,26 @@
-console.log("hello world");
+const createCard = (catNum) => {
+  const card = document.createElement("div");
+  card.classList.add("image-card-main");
+  card.innerHTML = `
+<div class="image-card-inner">
+  <div class="image-card-front">
+    ?
+  </div>
+  <div class="image-card-back">
+    <img
+    src="./images/cat${catNum}.jpeg"
+    alt="Cat"
+  />
+  </div>
+</div>`;
+
+  const mainGrid = document.getElementById("main-grid");
+  mainGrid.appendChild(card);
+  console.log(card);
+};
+
+const onStart = () => {
+  for (let i = 0; i < 16; i++) {
+    createCard((i % 8) + 1);
+  }
+};
