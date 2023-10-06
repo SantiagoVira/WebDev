e40186b3943afeeb8ee25f07cafaf9c1b3aaaeb7
SantiagoVira <santiagovira06@gmail.com>
2023-10-06 09:17:33 -0400

    work on project 3

---
+++ level3/script.js
@@ -0,0 +1,27 @@
+const weapons = {
+  computer: {
+    rocks: 2,
+    paper: 4,
+    scissors: 3,
+  },
+  my: {
+    rocks: 1,
+    paper: 2,
+    scissors: 3,
+  },
+};
+
+const updateNumbers = () => {
+  ["computer", "my"].forEach((player) => {
+    ["rocks", "paper", "scissors"].forEach((weapon) => {
+      console.log(player, weapon, weapons[player]);
+      document.getElementById(`${player}-${weapon}`).innerHTML =
+        weapons[player][weapon];
+    });
+  });
+};
+
+const startGame = () => {
+  console.log("hello");
+  updateNumbers();
+};
